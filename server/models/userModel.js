fa95254b5996df7d75953d1ffe385a676bd31dc3
Hoxtygen <hoxtygen@live.com>
2019-07-26 10:53:14 +0100

    Add all server files in a single folder

---
+++ server/models/userModel.js
@@ -0,0 +1,27 @@
+const db = require('../database/dbConfig');
+
+async function addUser(userData) {
+  return db('users')
+    .insert(userData);
+}
+
+async function findByUsername(username) {
+  return db('users').where({ username }).first();
+}
+
+async function findById(id) {
+  return db('users')
+    .where({ id })
+    .first();
+}
+
+async function find() {
+  return db('users').select();
+}
+
+module.exports = {
+  addUser,
+  findByUsername,
+  findById,
+  find,
+};
