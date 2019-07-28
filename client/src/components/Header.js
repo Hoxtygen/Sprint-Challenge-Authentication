e9f1d96e5192c47b9388d928238c5ab34fa53afd
Hoxtygen <hoxtygen@live.com>
2019-07-28 15:27:47 +0100

    Modify Header

--- client/src/components/Header.js
+++ client/src/components/Header.js
@@ -1,27 +1,61 @@
 import React from 'react';
 import { Link } from 'react-router-dom';
+import styled from 'styled-components'
 
 const Header = props => {
   return (
     <header>
-      <div className="header-inner">
+      <HeaderContainer>
         <div id="logo">
-          <h1>Jokes</h1>
+          <Link to = '/'><h1>Jokes</h1></Link>
         </div>
         <nav>
           <ul>
             <li>
-              <Link to="register">Sign Up</Link>
+              <Link to="register">Register</Link>
             </li>
             <li>
-              <Link to="login">Sign In</Link>
+              <Link to="login">Login</Link>
             </li>
           </ul>
         </nav>
-      </div>
+      </HeaderContainer>
     </header>
   )
 }
+
+const HeaderContainer = styled.div `
+  border: 1px solid red;
+  display: flex;
+  justify-content: space-between;
+  padding-right: 15px;
+  padding-left: 15px;
+  background-color: hsl(150, 50%, 69%);
+
+  nav {
+    display: flex;
+  }
+
+  ul {
+    display: flex;
+    padding: 0;
+    margin: 0;
+    list-style: none;
+    align-items: center;
+
+    a {
+      padding: 5px 10px;
+      margin-right: 2rem;
+      font-size: 1.2rem;
+      font-weight: 500;
+    }
+  }
+
+  a {
+    text-decoration: none;
+    color: #222;
+  }
+`
 
 
 export default Header
