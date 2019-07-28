b399045c624af91437b0e5181e1c0baf548f7aae
Hoxtygen <hoxtygen@live.com>
2019-07-28 13:19:45 +0100

    Add react application

---
+++ client/src/App.js
@@ -0,0 +1,13 @@
+import React from 'react';
+import logo from './logo.svg';
+import './App.css';
+
+function App() {
+  return (
+    <div className="App">
+      <h1>Hello people!! I'm here to show you many jokes around here</h1>
+    </div>
+  );
+}
+
+export default App;
